91e98b4aed93ea313e7119bdda8599aad72ac1e6
hung dam <hungdamduy@gmail.com>
2015-04-03 14:37:30 -0700

    Initial commit with django-example from openshift

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    # GETTING-STARTED: set your app name:
+    name='CenreWebGIS',
+    # GETTING-STARTED: set your app name:
+    version='1.0',
+    # GETTING-STARTED: set your app description:
+    description='Website for cenre with gis technology',
+    # GETTING-STARTED: set author name (your name):
+    author='Hung Dam',
+    # GETTING-STARTED: set author name (your name):
+    author_email='hungdamduy@gmail.com',
+    # GETTING-STARTED: set author name (your name):
+    url='http://www.python.org/sigs/distutils-sig/',
+    # GETTING-STARTED: define required django version:
+    install_requires=['Django<=1.8'],
+)
